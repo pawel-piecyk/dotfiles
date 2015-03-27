dd45e34fe9a367a34acc56f8aecea5a0a5bde452
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-03-27 15:36:50 +0100

    add zsh theme to custom themes

--- setup.sh
+++ setup.sh
@@ -14,9 +14,14 @@ echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
 echo "source ~/dotfiles/.zshrc" >> ~/.zshrc
 echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 
-if [ -f ~/.marks ];
+if [ ! -d ~/.marks ];
 then
     mkdir ~/.marks
 fi
 
-mv .oh-my-zsh/themes/* ~/.oh-my-zsh/custom/themes/
+if [ ! -d ~/.oh-my-zsh/custom/themes/ ]
+then
+    mkdir -p ~/.oh-my-zsh/custom/themes
+fi
+
+cp .oh-my-zsh/themes/* ~/.oh-my-zsh/custom/themes/
