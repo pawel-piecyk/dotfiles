3f09f998d63dfbf0692ae07fb28536e10e6b73e6
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-08-25 13:28:14 +0200

    small fix

--- setup.sh
+++ setup.sh
@@ -14,14 +14,7 @@ echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
 echo "source ~/dotfiles/.zshrc" >> ~/.zshrc
 echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 
-if [ ! -d ~/.marks ];
+if [ -f ~/.marks ];
 then
     mkdir ~/.marks
 fi
-
-if [ ! -d ~/.oh-my-zsh/custom/themes/ ]
-then
-    mkdir -p ~/.oh-my-zsh/custom/themes
-fi
-
-cp .oh-my-zsh/themes/* ~/.oh-my-zsh/custom/themes/
